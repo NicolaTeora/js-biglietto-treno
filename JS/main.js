56b2329ef6fcadd3180e68d8ba0bd08f43e7a01b
NicolaTeora <nikxodia@gmail.com>
2024-01-09 19:07:24 +0100

    feat : end calc

--- JS/main.js
+++ JS/main.js
@@ -2,16 +2,33 @@
 let age;
 let mileage;
 const priceKm = 0.21;
-let FristPrice
+let FirstPrice
 
 // assegnazione
-age = prompt('Inserisci la tua età');
+age = parseInt(prompt('Inserisci la tua età'));
 console.log(age);
 
-mileage = prompt('Inserisci i Kilometri da percorrere');
+mileage = parseInt(prompt('Inserisci i Kilometri da percorrere'));
 console.log(mileage);
 
-FristPrice = '' + mileage * priceKm + "€";
-console.log(FristPrice);
+FirstPrice = mileage * priceKm;
+console.log(FirstPrice);
 
-document.getElementById('specialprice').innerText = FristPrice;
+// Anteprima biglietto
+
+document.getElementById('specialprice').innerText = FirstPrice.toFixed(2) + '€';
+
+// calcolo dello sconto in base all'età
+let Discount
+
+if (age < 18){
+    Discount = FirstPrice - (FristPrice * 0.2);
+    
+} else if(age > 65) {
+    Discount = FirstPrice - (FristPrice * 0.4);
+}
+console.log(Discount);
+
+// prezzo scontato
+
+document.getElementById('discountPrice').innerText = Discount.toFixed(2) + '€';
