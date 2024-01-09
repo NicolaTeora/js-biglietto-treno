c59cef426bcfed7bc4bfd90c613f90da0cb38a55
NicolaTeora <nikxodia@gmail.com>
2024-01-09 17:29:29 +0100

    feat : ass var and test frist price

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,7 @@
 let age;
 let mileage;
 const priceKm = 0.21;
+let FristPrice
 
 // assegnazione
 age = prompt('Inserisci la tua età');
@@ -9,3 +10,8 @@ console.log(age);
 
 mileage = prompt('Inserisci i Kilometri da percorrere');
 console.log(mileage);
+
+FristPrice = '' + mileage * priceKm + "€";
+console.log(FristPrice);
+
+document.getElementById('specialprice').innerText = FristPrice;
