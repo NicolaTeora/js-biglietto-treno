4d402a9ffd759a6c22e3e7404b21c0b2b12ae28c
NicolaTeora <nikxodia@gmail.com>
2024-01-09 17:06:33 +0100

    feat : develop main.js

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+// dichiarazione variabili
+let age;
+let mileage;
+const priceKm = 0.21;
+
+// assegnazione
+age = prompt('Inserisci la tua età');
+console.log(age);
+
+mileage = prompt('Inserisci i Kilometri da percorrere');
+console.log(mileage);
